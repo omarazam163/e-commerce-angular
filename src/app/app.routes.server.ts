0575e735003f9d8375a31cb570c5ad2190d7b4c1
omar ahmed <50985259+omarazam163@users.noreply.github.com>
2025-02-27 20:29:23 +0200

    fixing the problem 5

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'single/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Server
-  }
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'checkout/:cartId',
+    renderMode: RenderMode.Server,
+  },
 ];
